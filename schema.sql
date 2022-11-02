730cd62cc6e3082a3334ce3f72829720f1f85d9b
CoderGirl <angi.adema@gmail.com>
2022-11-02 10:17:36 -0600

    updated schema.sql page

--- schema.sql
+++ schema.sql
@@ -4,29 +4,32 @@ CREATE DATABASE employee_tracker_db;
 USE employee_tracker_db;
 
 CREATE TABLE department (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   department_name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE employee_role (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
   department_id INT NOT NULL,
   PRIMARY KEY (ID)
-  FOREIGN KEY (department_id)
-  REFERENCES department(id)
-  ON DELETE SET NULL
+
+  -- FOREIGN KEY (department_id)
+  -- REFERENCES department(id)
+  -- ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
-  manager_id INT
-  FOREIGN KEY (employee_role_id)
-  REFERENCES employee_role(id)
-  ON DELETE SET NULL
+  manager_id INT,
+  PRIMARY KEY (id)
+
+  -- FOREIGN KEY (employee_role_id)
+  -- REFERENCES employee_role(id)
+  -- ON DELETE SET NULL
 );
