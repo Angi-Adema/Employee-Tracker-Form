88fcd1c692a1974bfe65b3c02aaf1dfc7ceb1431
CoderGirl <angi.adema@gmail.com>
2022-11-02 20:45:41 -0600

    updated comments in connection.js file

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,5 @@
 const mysql = require('mysql2');
 
-//DO I NEED TO HAVE THE PORT ABOVE OR LISTED IN THE CONNECTION FUNCTION BELOW???
 // Connect to database.
 const connection = mysql.createConnection(
     {
